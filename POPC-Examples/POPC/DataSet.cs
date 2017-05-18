4f3ef31be3ef8096176944d86d69b698b4908ae0
pepe78 <taraba.pepe@gmail.com>
2017-05-17 19:30:41 -0700

    adding example 3 to paper

---
+++ POPC-Examples/POPC/DataSet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContinuousPOPC
+{
+    public class DataSet
+    {
+        public List<DataRow> rows;
+
+        public DataSet(int numExpectedClusters, int numFeatures, int samplesPerCluser)
+        {
+            Random r = new Random();
+            rows = new List<DataRow>();
+
+            for (int i = 0; i < numExpectedClusters; i++)
+            {
+                double[] probs = new double[numFeatures];
+                for (int k = 0; k < numFeatures; k++)
+                {
+                    if (k < numExpectedClusters)
+                    {
+                        if (k == i)
+                        {
+                            probs[k] = 1.0;
+                        }
+                        else
+                        {
+                            probs[k] = 0.0;
+                        }
+                    }
+                    else
+                    {
+                        probs[k] = 0.5;
+                    }
+                }
+
+                for (int j = 0; j < samplesPerCluser; j++)
+                {
+                    bool[] feats = new bool[numFeatures];
+
+                    for (int k = 0; k < numFeatures; k++)
+                    {
+                        feats[k] = r.NextDouble() < probs[k];
+                    }
+                    DataRow dr = new DataRow();
+                    dr.features = feats;
+                    rows.Add(dr);
+                }
+            }
+        }
+    }
+}
---
+++ POPC-Examples/POPC/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ContinuousPOPC.clustering;
+
+namespace ContinuousPOPC
+{
+    class Program
+    {
+        static public Random r = new Random();
+
+        static void Main(string[] args)
+        {
+            RunExample3();
+        }
+
+        private static void RunExample3()
+        {
+            // scenario 3
+            int numExpctedClusters = 13;
+            int numFeatures = 50;
+            int samplesPerCluser = 300;
+            DataSet dataSet = new DataSet(numExpctedClusters, numFeatures, samplesPerCluser);
+            popc pop = new popc(dataSet);
+
+            //creates 13 clusters as expected
+
+            // results close expected 13
+            double vPOPC = popc.ComputeEval(pop.clusters, pop.countsAll);
+
+            if (Math.Abs(vPOPC - numExpctedClusters) > 0.1)
+            {
+                throw new Exception("theory not confirmed");
+            }
+
+            // pretend we know correct number of clusters
+            kmeans km = new kmeans(dataSet, pop.clusters.Count);
+            // even with knowledge of amount of correct clusters
+            // we get score close to 0 due to k-means clusters
+            // concentrates on noisy features and does not find
+            // ideal clusters, hence score close to 0
+            double vKMEANS = popc.ComputeEval(km.clusters, pop.countsAll);
+
+            if (Math.Abs(vKMEANS - 0.0) > 0.1)
+            {
+                throw new Exception("theory not confirmed");
+            }
+        }
+    }
+}
